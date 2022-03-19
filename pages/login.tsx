211a9e5e7ed6e466071f130c9eaa3faffed5d2bd
zwan0238@gmail.com <zwan0238@gmail.com>
2022-03-19 15:04:15 +1100

    signup and login

---
+++ pages/login.tsx
@@ -0,0 +1,88 @@
+import { Form, Input, Button, Checkbox, Row, Col, Space, Radio } from "antd";
+import { UserOutlined, LockOutlined } from "@ant-design/icons";
+import Link from "next/link";
+
+export default function SignInPage() {
+  const [form] = Form.useForm();
+  const onFinish = (values: any) => {
+    console.log("Received values of form: ", values);
+  };
+
+  return (
+    <>
+      <h1 className="signup">COURSE MANAGEMENT ASSISTANT</h1>
+      <Row justify="center">
+        <Col md={8} sm={24}>
+          <Form
+            name="normal_login"
+            className="login-form"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+          >
+            <Form.Item name="role" rules={[{ required: true }]}>
+              <Radio.Group>
+                <Radio.Button value="student">Student</Radio.Button>
+                <Radio.Button value="teacher">Teacher</Radio.Button>
+                <Radio.Button value="manager">Manager</Radio.Button>
+              </Radio.Group>
+            </Form.Item>
+
+            <Form.Item
+              name="username"
+              rules={[
+                {
+                  type: "email",
+                  message: "The input is not valid E-mail!",
+                },
+                {
+                  required: true,
+                  message: "Please input your E-mail!",
+                },
+              ]}
+            >
+              <Input
+                prefix={<UserOutlined className="site-form-item-icon" />}
+                placeholder="Please input email"
+              />
+            </Form.Item>
+            <Form.Item
+              name="password"
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
+            >
+              <Input
+                prefix={<LockOutlined className="site-form-item-icon" />}
+                type="password"
+                placeholder="Please input password"
+              />
+            </Form.Item>
+            <Form.Item>
+              <Form.Item name="remember" valuePropName="checked" noStyle>
+                <Checkbox>Remember me</Checkbox>
+              </Form.Item>
+            </Form.Item>
+
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                className="login-form-button"
+                block
+              >
+                Sign in
+              </Button>
+            </Form.Item>
+          </Form>
+
+          <Space>
+            <span>No account?</span>
+            <Link href="/">
+              <a>Sign up</a>
+            </Link>
+          </Space>
+        </Col>
+      </Row>
+    </>
+  );
+}
---
+++ pages/studentList.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+import { Button } from "antd";
+
+function StudentList() {
+  const getStudents = (e: any) => {
+    axios
+      .get(
+        "http://ec2-13-239-60-161.ap-southeast-2.compute.amazonaws.com:3001/api/students"
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <h1 className="signup">List</h1>
+      <Button type="primary" onClick={getStudents}>Get List</Button>
+    </>
+  );
+}
+
+export default StudentList;
