983bf7cfb7585f06c1a72975813fa7e85580a8ea
zwan0238@gmail.com <zwan0238@gmail.com>
2022-03-23 02:01:18 +1100

    table

--- pages/studentList.tsx
+++ pages/studentList.tsx
@@ -1,9 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Button } from "antd";
+import { Button, Table, Space } from "antd";
 
 function StudentList() {
-  const getStudents = (e: any) => {
+  const [dataSource, setDataSource] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+
+  useEffect(() => {
+    getStudents();
+  }, []);
+
+  const getStudents = () => {
     const config = {
       headers: {
         Authorization: "Bearer " + localStorage.getItem("token"),
@@ -11,19 +18,85 @@ function StudentList() {
     };
     axios
       .get(
-        "http://ec2-13-239-60-161.ap-southeast-2.compute.amazonaws.com:3001/api/students",
+        "http://ec2-13-239-60-161.ap-southeast-2.compute.amazonaws.com:3001/api/students?page=1&limit=299",
         config
       )
-      .then((res) => console.log(res))
+      .then((res) => {
+        setDataSource(res.data.data.students);
+        setTotalPages(res.data.data.total);
+        console.log(res.data.data.students)
+      })
       .catch((err) => console.log(err));
   };
+
+  const columns = [
+    {
+      title: "No.",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text: string) => <a>{text}</a>,
+    },
+    {
+      title: "Area",
+      dataIndex: "country",
+      key: "area",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Selected Curriculum",
+      dataIndex: "courses",
+      key: "courses",
+      render: (courses: any) => (
+        <span>
+          {courses.map((course: any) => {
+            return <div>{course.name}</div>;
+          })}
+        </span>
+      ),
+    },
+    {
+      title: "Student Type",
+      dataIndex: "type",
+      key: "type",
+      render: (type: any) => <span>{type.name}</span>,
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: () => (
+        <Space size="middle">
+          <a>Edit</a>
+          <a>Delete</a>
+        </Space>
+      ),
+    },
+  ];
 
   return (
     <>
       <h1 className="signup">List</h1>
-      <Button type="primary" onClick={getStudents}>
-        Get List
-      </Button>
+      <Space>
+        <Button type="primary" onClick={getStudents}>
+          Add
+        </Button>
+      </Space>
+      <Table
+        columns={columns}
+        dataSource={dataSource}
+        pagination={{
+          pageSize: 9,
+          total: totalPages,
+        }}
+      ></Table>
     </>
   );
 }
